4aa2f8a7d73534ee84c9f43163aafff0b0e6ef41
NoSocksForYou <kaappo.raivio@gmail.com>
2018-07-11 14:15:40 +0100

    added type hints and such

--- findbyid.py
+++ findbyid.py
@@ -5,6 +5,42 @@ class FindByIDFactory:
 
     def __init__(self, *args, **kwargs):
         cls = type(self)
+        if not hasattr(cls, "has_instantiated_before"):
+            cls.instances = []
+            cls.has_instantiated = True
+
+        self.ID = cls.getFreeID()
+        cls.instances.append(self)
+
+    @classmethod
+    def getFreeID(cls):
+        while True:
+            id = random.randint(10000, 100000)
+            if id not in cls.used_ids:
+                break
+
+        return id
+
+    @classmethod
+    def findByID(cls, ID):
+
+        for i in cls.instances:
+            if i.ID == ID:
+                return i
+        else:
+            raise Exception("Invalid ID!")
+
+    @classmethod
+    def getInstances(cls):
+        return cls.instances
+
+import random
+
+class FindByIDFactory:
+    used_ids = [] # Required for the getFreeID function to work properly.
+
+    def __init__(self, *args, **kwargs):
+        cls = type(self)
         if not hasattr(cls, "has_instantiated"):
             cls.instances = []
             cls.has_instantiated = True
